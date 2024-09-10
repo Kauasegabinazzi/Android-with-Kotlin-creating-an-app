b57e45187030805ba86b865c97e5b01bacab4d77
Kauasegabinazzi <101999858+Kauasegabinazzi@users.noreply.github.com>
2024-09-09 23:09:05 -0300

    3-commit

--- app/src/main/java/com/example/android_with_kotlin_creating_an_app/MainActivity.kt
+++ app/src/main/java/com/example/android_with_kotlin_creating_an_app/MainActivity.kt
@@ -1,7 +1,12 @@
 package com.example.android_with_kotlin_creating_an_app
 
 import android.app.Activity
+import android.os.Bundle
+import android.widget.Toast
 
 class MainActivity : Activity() {
-
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        Toast.makeText(this, "teste", Toast.LENGTH_SHORT).show()
+    }
 }
