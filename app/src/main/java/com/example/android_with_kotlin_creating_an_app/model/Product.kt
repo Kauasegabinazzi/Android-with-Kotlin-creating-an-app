d6a509f946bff5da1726d7e9cc286b6295d85e34
Kauasegabinazzi <101999858+Kauasegabinazzi@users.noreply.github.com>
2024-09-13 22:20:27 -0300

    11-commit

---
+++ app/src/main/java/com/example/android_with_kotlin_creating_an_app/model/Product.kt
@@ -0,0 +1,9 @@
+package com.example.android_with_kotlin_creating_an_app.model
+
+import java.math.BigDecimal
+
+class Product (
+    val name: String,
+    val description: String,
+    val values: BigDecimal
+)
--- app/src/main/java/com/example/android_with_kotlin_creating_an_app/recyclerViewAdapter/ProductsListAdapter.kt
+++ app/src/main/java/com/example/android_with_kotlin_creating_an_app/recyclerViewAdapter/ProductsListAdapter.kt
@@ -1,7 +1,19 @@
 package com.example.android_with_kotlin_creating_an_app.recyclerViewAdapter
 
+import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import com.example.android_with_kotlin_creating_an_app.model.Product
 
-class ProductsListAdapter : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+class ProductsListAdapter(private val products: List<Product>) : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int = products.size
+
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
 
 }
