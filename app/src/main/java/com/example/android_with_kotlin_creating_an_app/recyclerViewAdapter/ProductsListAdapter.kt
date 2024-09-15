3bf35508e18e6d1f0b7bb29befdf5694cc7b2f8d
Kauasegabinazzi <101999858+Kauasegabinazzi@users.noreply.github.com>
2024-09-14 23:23:58 -0300

    12-commit

--- app/src/main/java/com/example/android_with_kotlin_creating_an_app/recyclerViewAdapter/ProductsListAdapter.kt
+++ app/src/main/java/com/example/android_with_kotlin_creating_an_app/recyclerViewAdapter/ProductsListAdapter.kt
@@ -1,19 +1,27 @@
 package com.example.android_with_kotlin_creating_an_app.recyclerViewAdapter
 
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import com.example.android_with_kotlin_creating_an_app.R
 import com.example.android_with_kotlin_creating_an_app.model.Product
 
-class ProductsListAdapter(private val products: List<Product>) : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+class ProductsListAdapter(private val context: Context ,private val products: List<Product>) : RecyclerView.Adapter<ProductsListAdapter.ViewHolders>() {
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
-        TODO("Not yet implemented")
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ProductsListAdapter.ViewHolders {
+        val inflater = LayoutInflater.from(context)
+        val view = inflater.inflate(R.layout.produto_item,parent,false )
+        return ViewHolders(view)
     }
 
     override fun getItemCount(): Int = products.size
 
-    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+    override fun onBindViewHolder(holder: ProductsListAdapter.ViewHolders, position: Int) {
         TODO("Not yet implemented")
     }
+
+    class ViewHolders(view: View) : RecyclerView.ViewHolder(view)
 
 }
