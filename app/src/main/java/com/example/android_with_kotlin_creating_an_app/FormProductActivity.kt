2a84e59f77356df5046f274b90900127c82d7f0c
Kauasegabinazzi <101999858+Kauasegabinazzi@users.noreply.github.com>
2024-09-14 23:48:41 -0300

    14-commit

---
+++ app/src/main/java/com/example/android_with_kotlin_creating_an_app/FormProductActivity.kt
@@ -0,0 +1,12 @@
+package com.example.android_with_kotlin_creating_an_app
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+
+class FormProductActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_form_product)
+    }
+}
+
