3876cea0f488ecf4a5a004a3c3a818d72de07154
Kauasegabinazzi <101999858+Kauasegabinazzi@users.noreply.github.com>
2024-12-05 23:12:10 -0300

    25 - commit

--- app/src/main/java/com/example/android_with_kotlin_creating_an_app/activity/activity_lista_produtos.kt
+++ app/src/main/java/com/example/android_with_kotlin_creating_an_app/activity/activity_lista_produtos.kt
@@ -1,21 +1,17 @@
 package com.example.android_with_kotlin_creating_an_app.activity
 
 
-import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.android_with_kotlin_creating_an_app.Dao.ProductDao
 import com.example.android_with_kotlin_creating_an_app.FormProductActivity
 import com.example.android_with_kotlin_creating_an_app.R
-import com.example.android_with_kotlin_creating_an_app.model.Product
 import com.example.android_with_kotlin_creating_an_app.recyclerViewAdapter.ProductsListAdapter
 import com.google.android.material.floatingactionbutton.FloatingActionButton
-import java.math.BigDecimal
 
-class MainActivity : AppCompatActivity(R.layout.activity_main) {
+class ListaProdutosActivity : AppCompatActivity(R.layout.activity_lista_produtos) {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
     }
